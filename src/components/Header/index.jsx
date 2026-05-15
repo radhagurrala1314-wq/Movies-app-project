5d7ba484a904b309d21c5f26cff7e5df823490f6
Radha <radhagurrala1314@gmail.com>
2026-05-15 11:28:12 +0530

    updated header  page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,62 +1,5 @@
-// import Cookies from 'js-cookie'
-// import {useNavigate, Link} from 'react-router-dom'
-
-// import './index.css'
-
-// const Header = () => {
-//   const navigate = useNavigate()
-
-//   const onClickLogout = () => {
-//     Cookies.remove('jwt_token')
-//     navigate('/login', {replace: true})
-//   }
-
-//   return (
-//     <div className="home_container">
-//       <nav className="nav_links">
-
-//         <div className="logo_section">
-//           <img
-//             src="https://tse3.mm.bing.net/th/id/OIP.xGODOGRV6qSXnzonv6laxwHaEc?pid=Api&P=0&h=180"
-//             alt="Movie_Hub"
-//             height="80"
-//             width="130"
-//             className="image"
-//           />
-//         </div>
-
-//         <ul className="unoder_links">
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-
-//           <li>
-//             <Link to="/movies">Movies</Link>
-//           </li>
-
-//           <li>
-//             <Link to="/shows">Shows</Link>
-//           </li>
-
-//           <li>
-//             <Link to="/trend">TrendingMovie</Link>
-//           </li>
-//         </ul>
-
-//         <button className="signout" onClick={onClickLogout}>
-//           Sign out
-//         </button>
-
-//       </nav>
-//     </div>
-//   )
-// }
-
-// export default Header
 import Cookies from 'js-cookie'
 import {useNavigate, Link} from 'react-router-dom'
-import { CiSearch } from "react-icons/ci"
-import { FaRegBell } from "react-icons/fa6"
 
 import './index.css'
 
@@ -100,22 +43,9 @@ const Header = () => {
           </li>
         </ul>
 
-        <div className="right_section">
-          <div className="search_container">
-            <input
-              type="search"
-              placeholder="Search movies..."
-              className="search_input"
-            />
-            <CiSearch className="search_icon" />
-          </div>
-
-          <FaRegBell className="bell_icon" />
-
-          <button className="signout" onClick={onClickLogout}>
-            Sign out
-          </button>
-        </div>
+        <button className="signout" onClick={onClickLogout}>
+          Sign out
+        </button>
 
       </nav>
     </div>
